a5f9b9785f55d4e5145e1fe0040fc1cb9335849b
Mikhail <mm@r-c.ca>
2018-12-18 00:58:43 -0500

    zigzag

--- ZigZag/ZigZag.js
+++ ZigZag/ZigZag.js
@@ -7,6 +7,19 @@ let test=require('../test.js').TestExt;
  * @return {string}
  */
 var convert = function(s, numRows) {
+    let r='';
+    let len=s.len;
+    let ind=0;
+    let inc1=Math.ceil(len / numRows);
+    let inc2=2*numRows-2;
+
+    let jump=0;
+    for(let i=0;i<len;i+=inc1)
+        for(let j=i;j<len;j+=inc2)
+    while (ind<len){
+        r+=s[ind];
+        jump=ind+inc;
+    }
     return 1;
 };
 let s=[
