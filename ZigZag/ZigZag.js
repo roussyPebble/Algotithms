3fc19e0773ae50b1ef5fcf0b3b46ba74e91e666c
Mikhail Michoura <Mikhail.Michoura@RADIO-CANADA.CA>
2018-12-18 19:52:57 -0500

    zigzag solution

--- ZigZag/ZigZag.js
+++ ZigZag/ZigZag.js
@@ -8,21 +8,32 @@ let test=require('../test.js').TestExt;
  */
 var convert = function(s, numRows) {
     let r='';
-    let len=s.len;
+    let len=s.length;
     let ind=0;
-    let inc1=Math.ceil(len / numRows);
     let inc2=2*numRows-2;
-
+    let inc1=Math.floor(len / inc2)+1;
+    //console.log(`inc1=${inc1}`);
+    //console.log(`inc2=${inc2}`);
     let jump=0;
-    for(let i=0;i<len;i+=inc1)
-        for(let j=i;j<len;j+=inc2)
-    while (ind<len){
-        r+=s[ind];
-        jump=ind+inc;
+    for(let i=0;i<numRows;i++){
+        for(let j=0,k=inc2,t=0;t<inc1;j+=inc2,k+=inc2,t++){
+            //console.log(`i=${i}, j=${j}, i+j=${i+j}`);
+            if ( i+j < len)
+                r+=s[i+j];
+                let exp=k-i;
+            if(i<numRows-1&&i>0&&exp<len){
+                
+                //console.log(`---------- p=${i}, k=${k}, p+k=${exp} `);
+                r+=s[exp];
+            }
+        }
     }
-    return 1;
+       
+    return r;
 };
 let s=[
-    {in:['PAYPALISHIRING',3],expected:'PAHNAPLSIIGYIR'}
+    {in:['PAYPALISHIRING',3],expected:'PAHNAPLSIIGYIR'},
+    {in:['PAYPALISHIRING',4],expected:'PINALSIGYAHRPI'},
+    {in:['PITYFSAIUKPHIFWZ',5],expected:'PUIIKZTAPWYSHFFI'}
 ];
 test(s,convert);
