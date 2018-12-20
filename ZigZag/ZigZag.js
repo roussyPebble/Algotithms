457e972a2b7cc4581561db18eb3d2c4fcd5714b4
Mikhail Michoura <Mikhail.Michoura@RADIO-CANADA.CA>
2018-12-20 17:09:29 -0500

    ZigZag Accepted with 35%

--- ZigZag/ZigZag.js
+++ ZigZag/ZigZag.js
@@ -15,6 +15,7 @@ var convert = function(s, numRows) {
     //console.log(`inc1=${inc1}`);
     //console.log(`inc2=${inc2}`);
     let jump=0;
+    if (numRows===1 || len<3) return s;
     for(let i=0;i<numRows;i++){
         for(let j=0,k=inc2,t=0;t<inc1;j+=inc2,k+=inc2,t++){
             //console.log(`i=${i}, j=${j}, i+j=${i+j}`);
@@ -34,6 +35,9 @@ var convert = function(s, numRows) {
 let s=[
     {in:['PAYPALISHIRING',3],expected:'PAHNAPLSIIGYIR'},
     {in:['PAYPALISHIRING',4],expected:'PINALSIGYAHRPI'},
-    {in:['PITYFSAIUKPHIFWZ',5],expected:'PUIIKZTAPWYSHFFI'}
+    {in:['PITYFSAIUKPHIFWZ',5],expected:'PUIIKZTAPWYSHFFI'},
+    {in:['',1],expected:''},
+    {in:['SDR',1],expected:'SDR'},
+    {in:['SDRF',2],expected:'SRDF'},
 ];
 test(s,convert);
