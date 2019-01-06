adf15c18592cab77ef04fb3b136644b3c21a331e
Mikhail <mm@r-c.ca>
2019-01-06 14:02:18 -0500

    solution accepted for Least Operators to Express Number

--- contests/116/Least-Operators.js
+++ contests/116/Least-Operators.js
@@ -9,24 +9,31 @@
  */
 let test=require('../../test.js').TestExt;
 var leastOpsExpressTarget = function(x, target) {
-    let count=0;
-    let f=target;
-    let i=0;
-    if (target===0) return 3;
-    do{
-        let k=1;
-        let p=power(x,f);
-        console.log(`p=${p}, f=${f}, next f=${f-Math.pow(x,p)}`);
-        f=Math.abs(f-Math.pow(x,p));
-        if(p===0) p=2;
-        count+=p-1;
-        if(f>0) count++;
-        console.log(`count=${count}`);
-    }while(f>0);
-    return count;
+    let solutions=[];
+    if (target===0) return 1;
+    recur(0,x,target);
+    return Math.min(...solutions);
+
     function power(x,t){
         let s=Math.log(t)/Math.log(x);
-        return Math.trunc(s) + (s%1>0.76?1:0);
+        let k=Math.trunc(s);
+        return (k===Math.round(s))?[k]:[k,k+1];
+    }
+    function recur(count,x,target){
+        let p=power(x,target);
+        p.forEach(a => {
+            let c=count;
+            let f=Math.abs(target-Math.pow(x,a));
+            if(f<target){
+                if(a===0) a=2;    
+                c+=a-1;
+                if(f>0){ 
+                    recur(++c,x,f);
+                }else{
+                    solutions.push(c);
+                }
+            }
+        });
     }
 };
 let s=[
@@ -35,7 +42,8 @@ let s=[
     {in:[3,17],expected:5,show:true},
     {in:[3,20],expected:6,show:true},
     {in:[3,26],expected:4,show:true},
-    {in:[9,14],expected:10,show:true}
+    {in:[9,14],expected:9,show:true},
+    {in:[29,0],expected:1,show:true}
 ];
-//test(s,leastOpsExpressTarget);
-test([s[3]],leastOpsExpressTarget);
+test(s,leastOpsExpressTarget);
+//test([s[3]],leastOpsExpressTarget);
