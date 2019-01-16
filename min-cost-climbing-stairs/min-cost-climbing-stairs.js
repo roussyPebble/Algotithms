3a29d55b5ddca693b3ae213672608a64e34d9d0a
Mikhail Michoura <Mikhail.Michoura@RADIO-CANADA.CA>
2019-01-16 17:58:10 -0500

    minCostClimbingStairs half solution

--- min-cost-climbing-stairs/min-cost-climbing-stairs.js
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-    return 0;
+    let min=Number.MAX_SAFE_INTEGER;
+    step(0,0);
+    step(1,cost[0]);
+    return min;
+
+    function step(i,c){
+        if(i===cost.length-1) {
+            min=Math.min(min,c);
+        }else{
+            if (i<cost.length-1) step(i+1,c+cost[i+1]);
+            if (i<cost.length-2) step(i+2,c+cost[i+2]);
+        }
+    }
 };
 
 
