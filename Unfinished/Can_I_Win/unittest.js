b130b7dcfccd4a5c6d3d1a6bd3928d353907d9fc
Mikhail Michoura <Mikhail.Michoura@RADIO-CANADA.CA>
2019-01-29 18:30:02 -0500

    add can i win

---
+++ Unfinished/Can_I_Win/unittest.js
@@ -0,0 +1,24 @@
+import solution from  "./Can_I_Win";
+import {TestExt} from "../../test";
+
+let s=[
+    {in:[10,40],expected:false,show:true},
+    {in:[5,10],expected:true,show:true},
+    {in:[5,11],expected:false,show:true},
+    {in:[5,12],expected:true,show:true},
+    {in:[5,13],expected:true,show:true},
+    {in:[6,10],expected:true,show:true},
+    {in:[6,11],expected:true,show:true},
+    {in:[6,12],expected:true,show:true},
+    {in:[6,13],expected:true,show:true},
+    {in:[6,14],expected:false,show:true},
+    {in:[6,15],expected:true,show:true},
+    {in:[6,16],expected:true,show:true},
+    {in:[6,17],expected:true,show:true},
+    {in:[6,18],expected:false,show:true},
+    {in:[7,14],expected:true,show:true},
+    {in:[7,15],expected:false,show:true},
+    {in:[5,6],expected:false,show:true},
+    
+]
+TestExt(s,solution);
